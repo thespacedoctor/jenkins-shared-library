8176ef67e9f6e0a1643b60f98751e0555574c32b
Dave <d.r.young@qub.ac.uk>
2020-02-14 11:04:34 +0000

    x

---
+++ vars/logs.groovy
@@ -0,0 +1,20 @@
+def debug(message) {
+    echo "DEBUG: ${message}"
+}
+
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARNING: ${message}"
+}
+
+def error(message) {
+    echo "ERROR: ${message}"
+}
+
+def critical(message) {
+    echo "CRITICAL: ${message}"
+}
+
