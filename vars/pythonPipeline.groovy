ea966901941a0f53309f2f284f9efa5e94b4d466
Dave <d.r.young@qub.ac.uk>
2020-02-19 16:06:46 +0000

    x

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -162,4 +162,6 @@ String coverageReportUrl() {
 String slackMessage(status) {
     return "<${env.OVERVIEW_URL}|${env.REPO_NAME}> / <${env.BUILD_URL}|${env.BRANCH_NAME}>\n\tBuild ${env.BUILD_NUMBER} ${status}\n\t<${env.COVERAGE_URL}|Coverage Rate = ${env.COVERAGE_RATE}>"
 }
-String coverageRate = new File('reports/coverage.txt').text
+String coverageRate() {
+    return new File('reports/coverage.txt').text
+}
