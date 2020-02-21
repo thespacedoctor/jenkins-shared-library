9179d4c418cbd2f930a76961c26d73a1c8e814ee
Dave <d.r.young@qub.ac.uk>
2020-02-21 08:58:09 +0000

    x

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -58,7 +58,7 @@ def call(body) {
                     }
                 }
                 steps {
-                    sh '''ashasjs
+                    sh '''echo "nice"
                        '''
                 }
             }
