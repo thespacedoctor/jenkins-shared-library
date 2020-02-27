4228c7e942196da5c6f13d5fe1094b9944d80376
Dave <d.r.young@qub.ac.uk>
2020-02-27 16:39:05 +0000

    x

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -286,6 +286,7 @@ def slackMessage(status) {
         message = "REPO: *<${env.OVERVIEW_URL}|${env.REPO_NAME}>*\nBRANCH: *<${env.BUILD_URL}|${env.BRANCH_MATCH}>*\nBUILD: *#${env.BUILD_NUMBER}*\nSTATUS: *${status}*"
     } else if(status == "running") {
         badgeImage = "https://raster.shields.io/static/v1?label=build&message=running&color=blueviolet"
+        badgeImage = "https://raster.shields.io/badge/build-failed-red.png"
         message = "REPO: *<${env.OVERVIEW_URL}|${env.REPO_NAME}>*\nBRANCH: *<${env.BUILD_URL}|${env.BRANCH_MATCH}>*\nBUILD: *#${env.BUILD_NUMBER}*\nSTATUS: *${status}*"  
     } else {
         def crStr = readFile('reports/coverage.txt').trim()
