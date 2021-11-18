ad29ea49c84c73ee75eb7960a4a26e113ff2fc71
Dave <d.r.young@qub.ac.uk>
2021-11-18 13:32:32 +0000

    x

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -16,7 +16,7 @@ def call(body) {
     body.delegate = pipelineParams
     body()
     def buildBadge = addEmbeddableBadgeConfiguration()
-    def commitHash = checkout(scm).GIT_COMMIT
+    def commitHash = ""
     pipeline {
 
         agent any
@@ -57,6 +57,7 @@ def call(body) {
                         slackSend(message: "${env.REPO_NAME} - ${env.BRANCH_MATCH} build running".toLowerCase(), blocks: slackMessage('running'))
                         buildBadge.setStatus('running')
                     }
+                    commitHash=checkout(scm).GIT_COMMIT
                     println "hash = ${commitHash}"
                 }
             }
