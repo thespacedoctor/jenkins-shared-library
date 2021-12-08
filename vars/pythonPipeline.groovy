fe1dd4902c152aa9e1447818ef8a24c7042a5ab1
Dave <d.r.young@qub.ac.uk>
2021-12-08 20:50:27 +0000

    x

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -86,9 +86,9 @@ def call(body) {
 
                         // Determine actual PR commit, if necessary
                         sh 'mkdir -p tmp'
-                        sh 'git rev-parse HEAD | git log --pretty=%P -n 1 --date-order > /tmp/MERGE_COMMIT_PARENTS'
-                        sh 'cat /tmp/MERGE_COMMIT_PARENTS'
-                        merge_commit_parents = readFile('/tmp/MERGE_COMMIT_PARENTS').trim()
+                        sh 'git rev-parse HEAD | git log --pretty=%P -n 1 --date-order > tmp/MERGE_COMMIT_PARENTS'
+                        sh 'cat tmp/MERGE_COMMIT_PARENTS'
+                        merge_commit_parents = readFile('tmp/MERGE_COMMIT_PARENTS').trim()
                         if (merge_commit_parents.length() > 40) {
                             echo 'More than one merge commit parent signifies that the merge commit is not the PR commit'
                             echo "Changing git_commit from '${env.GIT_COMMIT}' to '${merge_commit_parents.take(40)}'"
